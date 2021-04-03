c69c0fec03bb78b439e15bfa27ff814a10fcd3b1
Nur Hidayat <hidayat365@gmail.com>
2021-04-03 12:41:53 +0700

    update composer

--- config/web.php
+++ config/web.php
@@ -6,6 +6,10 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
     'components' => [
         'request' => [
             'cookieValidationKey' => 'cookieValidationKey',
